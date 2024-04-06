d271ef8b7f01d2fffc4fbafaabecfeb431683f7a
Rick <rick8036@gmail.com>
2024-04-06 21:19:16 +0800

    pages資料增加id 欄位

--- routes/pagesRouter.js
+++ routes/pagesRouter.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const { v4: uuidv4 } = require('uuid');
 
 //pages
 const pages = [
-    {'name':'Home','url':'/'},
-    {'name':'MUI','url':'/mui'},
-    {'name':'Three','url':'/box_three'},
-    {'name':'Example','url':'/example'}
+    {id: uuidv4(), 'name':'Home','url':'/'},
+    {id: uuidv4(), 'name':'MUI','url':'/mui'},
+    {id: uuidv4(), 'name':'Three','url':'/box_three'},
+    {id: uuidv4(), 'name':'Example','url':'/example'}
 ];
 
 // Get all pages
