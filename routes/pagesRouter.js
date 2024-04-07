d00a415abb6c998e4aa6e222addeb8401ce4bad0
Rick <rick8036@gmail.com>
2024-04-07 20:24:58 +0800

    新增路由 (尚未實作)

--- routes/pagesRouter.js
+++ routes/pagesRouter.js
@@ -5,7 +5,16 @@ const pageController = require('../controllers/pagesController');
 // Get all pages
 router.get('/', pageController.getAllPages);
 
+// Get one page
+router.get('/:id', (req, res) => { res.status(500).send('NotImplement Exception')});
+
 // Create page
 router.post('/', pageController.createPage);
+
+// Update page
+router.put('/:id', (req, res) => { res.status(500).send('NotImplement Exception')});
+
+// Delete page
+router.delete('/:id', (req, res) => { res.status(500).send('NotImplement Exception')});
 
 module.exports = router;
