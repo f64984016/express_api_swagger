2eb186750c6681a2063e835a94ca4a041f19ad7b
Rick <rick8036@gmail.com>
2024-04-17 17:10:36 +0800

    Add swagger comment by Get All method

--- routes/pagesRouter.js
+++ routes/pagesRouter.js
@@ -3,7 +3,20 @@ var router = express.Router();
 const pageController = require('../controllers/pagesController');
 
 // Get all pages
-router.get('/', pageController.getAllPages);
+router.get(
+    '/',
+    /* 	#swagger.tags = ['Page']
+        #swagger.description = '取得全部 pages' */
+    /* #swagger.responses[200] = { 
+      schema: [
+        {
+            "id": "fda7cb56-84d0-4b3b-accf-8fdf1e56257d",
+            "name": "Home",
+            "url": "/"
+        }]
+     } */
+    pageController.getAllPages
+);
 
 // Get one page
 router.get('/:id', pageController.getPagesById);
