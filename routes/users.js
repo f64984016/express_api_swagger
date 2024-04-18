0657da838a4f2145f002678f917bde37e9461955
Rick <rick8036@gmail.com>
2024-04-18 10:48:10 +0800

    Implement User Register

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,40 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  // #swagger.ignore = true
-  res.send('respond with a resource');
+const router = express.Router();
+
+const users = {
+  "f64984016@mail.com":{
+    "username": "rick",
+    "password": ""
+  }
+};
+
+// 1.Register
+router.post('/signup',async(req, res) => {
+  const {email, username, password} = req.body;
+  console.log(email, username, password);
+
+  if(users[email]) {
+    res.status(400).send('User already exist.');
+  }
+  // 1-1 bcrypt password
+  const hashpassword = await bcrypt.hash(password, 10)
+  console.log(hashpassword);
+
+  // 1-2 Password Storage
+  users[email] = {
+    password: hashpassword,
+    username
+  };
+  console.log(users);
+  // 1-3 Response
+  res.status(201).send({message:'Register Successfully!'});
 });
+
+// 2.Login
+
+// 3.AuthenticateUser
 
 module.exports = router;
