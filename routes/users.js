108f1a45b6adf2411e26bee64af14cc9a281ab00
Rick <rick8036@gmail.com>
2024-04-22 14:56:48 +0800

    撰寫API文件

--- routes/users.js
+++ routes/users.js
@@ -14,11 +14,30 @@ const users = {
 };
 
 // 1.Register
-router.post('/signup',async(req, res) => {
+router.post('/signup',
+    /* 	#swagger.tags = ['User']
+        #swagger.description = '註冊' */
+    /*	#swagger.parameters['body'] = {
+        in: 'body',
+        description: '註冊資訊',
+        required: true,
+        schema: {
+            "email": "電子郵件",
+            "username": "使用者名稱",
+            "password": "密碼"
+          }
+    } */
+    /* #swagger.responses[201] = { 
+          schema: { "message": "Register Successfully!" }
+        } */
+    /* #swagger.responses[400] = { 
+          schema: { "message": "User already exist." }
+        } */
+  async(req, res) => {
   const {email, username, password} = req.body;
   // Validation
   if(users[email]) {
-    res.status(400).send('User already exist.');
+    res.status(400).send({message: 'User already exist.'});
   }
   // 1-1 bcrypt password
   const hashpassword = await bcrypt.hash(password, 10)
@@ -32,7 +51,30 @@ router.post('/signup',async(req, res) => {
 });
 
 // 2.Login
-router.post('/login',async(req, res) => {
+router.post('/login', 
+    /* 	#swagger.tags = ['User']
+        #swagger.description = '登入' */
+    /*	#swagger.parameters['body'] = {
+        in: 'body',
+        description: '登入資訊',
+        required: true,
+        schema: {
+            "email": "電子郵件",
+            "password": "密碼"
+          }
+    } */
+    /* #swagger.responses[401] = { 
+          schema: { "error": "User Not Found." }
+        } */
+    /* #swagger.responses[401] = { 
+          schema: { "error": "Login Error." }
+        } */
+    /* #swagger.responses[200] = { 
+          schema: { 
+            "message": "Success",
+            "token": "token的內容" }
+        } */
+        async(req, res) => {
   const {email, password} = req.body;
   // 2-1 Verify User Exist
   const user = users[email];
@@ -56,7 +98,30 @@ router.post('/login',async(req, res) => {
 });
 
 // 3.Authentication
-router.get('/profile', (req, res) => {
+router.get('/profile', 
+    /* 	#swagger.tags = ['User']
+        #swagger.description = '認證' */   
+    /*  #swagger.responses[200] = { 
+          schema: {
+              "message": "Success",
+              "user": {
+                  "email": "電子郵件地址",
+                  "username": "使用者名稱",
+                  "iat": 123456
+              }
+          }
+        } */
+    /*  #swagger.responses[401] = { 
+          schema: {
+              "error": "No sign in"
+          }
+        } */
+    /*  #swagger.responses[403] = { 
+          schema: {
+              "error": "Auth fail"
+          }
+        } */
+        (req, res) => {
   const token = req.headers['authorization'];
   // 3-1 Verify whether request contain token 
   if (!token) {
